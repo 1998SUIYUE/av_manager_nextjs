e05e9bc9249276ad4d1aab33abc986a516f5003d
1998SUIYUE <593180497@qq.com>
2025-06-08 19:53:15 +0800

    优化打印

--- src/lib/movieMetadataCache.ts
+++ src/lib/movieMetadataCache.ts
@@ -18,9 +18,7 @@ let _cache: MovieMetadata[] | null = null;
 
 // 日志时间戳工具函数
 function logWithTimestamp(...args: unknown[]) {
-  const now = new Date();
-  const ts = now.toISOString().replace('T', ' ').replace('Z', '');
-  console.log(`[${ts}]`, ...(args as unknown[]));
+  console.log(`[${new Date().toISOString().replace('T', ' ').replace('Z', '')}]`, ...(args as unknown[]));
 }
 function warnWithTimestamp(...args: unknown[]) {
   const now = new Date();
